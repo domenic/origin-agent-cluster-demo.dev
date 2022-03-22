4e52f1a6ed088135cd25ba2c1e8259925881d8ce
Domenic Denicola <d@domenic.me>
2022-03-22 11:26:43 -0400

    Add some tests for ?0 and no-header

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This repository hosts the demos available at <https://origin-isolation-test.com/
 * <https://origin-isolation-test.com/mismatch-1.html>
 * <https://origin-isolation-test.com/mismatch-2.html>
 * <https://origin-isolation-test.com/bad-header.html>
+* <https://origin-isolation-test.com/no-header.html>
+* <https://origin-isolation-test.com/explicit-false.html>
 * <https://origin-isolation-test.com/resizing.html>
 
 (The domain name is "origin isolation" because that was the old name of the feature; see [whatwg/html#6214](https://github.com/whatwg/html/pull/6214).)
--- outer-script.mjs
+++ outer-script.mjs
@@ -1,6 +1,7 @@
 const send = document.querySelector("#send");
 const output = document.querySelector("#output");
 const syncScript = document.querySelector("#sync-script");
+const subframeSelector = document.querySelector("#subframe-selector");
 
 send.onclick = async () => {
   const module = await WebAssembly.compileStreaming(fetch("simple.wasm"));
@@ -14,6 +15,27 @@ syncScript.onclick = () => {
     output.textContent += `Failed to script the iframe: ${e.message}\n`;
   }
 };
+
+if (subframeSelector) {
+  subframeSelector.onchange = () => {
+    replaceSubframe(subframeSelector.value);
+    location.hash = subframeSelector.value;
+  };
+
+  if (location.hash) {
+    const frameURL = location.hash.substring(1);
+    replaceSubframe(frameURL);
+    subframeSelector.value = frameURL;
+  } else {
+    subframeSelector.onchange();
+  }
+}
+
+function replaceSubframe(frameURL) {
+  const iframe = document.createElement("iframe");
+  iframe.src = frameURL;
+  document.querySelector("iframe").replaceWith(iframe);
+}
 
 output.textContent += `window.originAgentCluster: ${window.originAgentCluster}\n`;
 
